36667cdb6cefb3cb4ad2876a4ebdc47a2818d6f9
XZhou <XZhou@DESKTOP-V3B5PMT>
2018-08-12 21:26:25 +1000

    test for add and print

--- COMP2511-Assign1-18s2/ass1/HotelBookingSystem.java
+++ COMP2511-Assign1-18s2/ass1/HotelBookingSystem.java
@@ -6,9 +6,24 @@ public class HotelBookingSystem {
 
 	private ArrayList<Hotel> hotelList;
 	
+	public HotelBookingSystem() {
+		this.hotelList = new ArrayList<Hotel>();
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		HotelBookingSystem HBSys = new HotelBookingSystem();
+		HBSys.addHotelRoom("Test1", "101", "2");
+		HBSys.addHotelRoom("Test1", "102", "1");
+		HBSys.addHotelRoom("Test1", "103", "3");
+		HBSys.addHotelRoom("Test2", "101", "1");
+		HBSys.addHotelRoom("Test2", "201", "1");
+		HBSys.addHotelRoom("Test2", "202", "2");
+		HBSys.addHotelRoom("Test3", "101", "2");
+		HBSys.addHotelRoom("Test3", "103", "3");
+		HBSys.printHotel("Test1");
+		HBSys.printHotel("Test2");
+		HBSys.printHotel("Test3");
 	}
 	
 	public boolean addHotelRoom(String hotelName, String roomNumber, String capacity) {
