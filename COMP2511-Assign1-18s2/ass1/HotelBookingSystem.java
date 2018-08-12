3cf35ab34f0fd5335298e1a2b83499a1f34b7222
XZhou <XZhou@DESKTOP-V3B5PMT>
2018-08-12 20:28:35 +1000

    add variables and methods to Order class

--- COMP2511-Assign1-18s2/ass1/HotelBookingSystem.java
+++ COMP2511-Assign1-18s2/ass1/HotelBookingSystem.java
@@ -2,6 +2,8 @@ package ass1;
 
 public class HotelBookingSystem {
 
+	private ArrayList<Hotel> hotelList;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- COMP2511-Assign1-18s2/ass1/Order.java
+++ COMP2511-Assign1-18s2/ass1/Order.java
@@ -1,5 +1,35 @@
 package ass1;
 
+import java.time.LocalDate;
+
 public class Order {
 
+	private String name;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	private int length;
+	
+	public Order(String name, LocalDate startDate, int length) {
+		super();
+		this.name = name;
+		this.startDate = startDate;
+		this.endDate = this.startDate.plusDays(length-1);
+		this.length = length;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public LocalDate getStartDate() {
+		return this.startDate;
+	}
+	
+	public LocalDate getEndDate() {
+		return this.endDate;
+	}
+	
+	public int getLength() {
+		return this.length;
+	}
 }
